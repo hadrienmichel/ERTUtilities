18ace7402d449a9e4e094c1d88397fa08599ed26
hadrienmichel <hadrien.michel@uliege.be>
2021-07-12 17:20:37 +0200

    3D protocols for Tom

--- Protocols3D.py
+++ Protocols3D.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.core import multiarray
 from Protocols2D import CreateGradient, CreateDDN
-from Utilities import SaveArrayTXT
+from Utilities import SaveArrayTXT, SortArray
 from copy import deepcopy
 
 def CreateMultiLineGradient(nLines:int=4, nElecLine:int=32, s:int=7):
@@ -14,7 +14,7 @@ def CreateMultiLineGradient(nLines:int=4, nElecLine:int=32, s:int=7):
             array[:,2:] += lineMeas*nElecLine
             multiGradient = np.vstack([multiGradient,array])
     multiGradient = multiGradient[1:,:]
-    return multiGradient
+    return SortArray(multiGradient)
 
 def CreateMultiLineDDN(nLines:int=4, nElecLine:int=32, nMax:int=6):
     simpleDDN = CreateDDN(nElec = nElecLine, nMax = nMax, parralelizeMin = nMax)
@@ -26,13 +26,74 @@ def CreateMultiLineDDN(nLines:int=4, nElecLine:int=32, nMax:int=6):
             array[:,2:] += lineMeas*nElecLine
             multiDDN = np.vstack([multiDDN,array])
     multiDDN = multiDDN[1:,:]
-    return multiDDN
+    return SortArray(multiDDN)
+
+def CreateTom(testCase:int=0):
+    nElecLine = 32
+    s = 7
+    simpleGradient = CreateGradient(nElec=nElecLine, s=s)
+    ABInj = np.unique(simpleGradient[:,:2],axis=0)
+    if testCase == 0:
+        # Case with full 3D array:
+        return CreateMultiLineGradient()
+    elif testCase == 1:
+        # Case with 2 injection lines in the center
+        # We only inject on the middle lines and measure fully on those.
+        # Cross line measurements are performed sparsly on the other lines
+        FullArray = np.asarray([0, 0, 0, 0])
+        injectionLines = [1, 2]
+        crossLines = [[1, 2, 3], [0, 2, 3], [0, 1, 3], [0, 1, 2]]
+        for lineInj in injectionLines:
+            array = deepcopy(simpleGradient)
+            array[:,:2] += lineInj*nElecLine
+            array[:,2:] += lineInj*nElecLine
+            FullArray = np.vstack([FullArray, array])
+            for AB in ABInj:
+                A = AB[0]
+                B = AB[1]
+                a = int((B-A)/(s+2))
+                M = A + 4*a #int((B-A)/2 - a/2)
+                N = M + a #int((B-A)/2 + a/2)
+                for lineMeas in crossLines[lineInj]:
+                    ACurr = A + lineInj*nElecLine
+                    BCurr = B + lineInj*nElecLine
+                    MCurr = M + lineMeas*nElecLine
+                    NCurr = N + lineMeas*nElecLine
+                    FullArray = np.vstack([FullArray, [ACurr, BCurr, MCurr, NCurr]])
+        FullArray = FullArray[1:,:]
+    elif testCase==2:
+        # Case with injection on a diagonal on outer lines and measures cross-lined in the middle
+        simpleGradient10 = CreateGradient(nElec= nElecLine, s=10)
+        # From left to right:
+        array1 = deepcopy(simpleGradient10)
+        array1[:,1] += 3*nElecLine
+        array1[:,2] += 1*nElecLine
+        array1[:,3] += 2*nElecLine
+        # From right to left:
+        array2 = deepcopy(simpleGradient10)
+        array2[:,0] += 3*nElecLine
+        array2[:,2] += 2*nElecLine
+        array2[:,3] += 1*nElecLine
+
+        FullArray = np.vstack([array1, array2])
+
+    return SortArray(FullArray)
 
 if __name__ == "__main__":
-    multiDDN = CreateMultiLineGradient()
-    print('Default 4 lines Gradient array (length = {}):'.format(len(multiDDN)))
-    print(multiDDN)
+    multiGradient = CreateMultiLineGradient()
+    print('Default 4 lines Gradient array (length = {}):'.format(len(multiGradient)))
+    print(multiGradient)
     multiDDN = CreateMultiLineDDN()
     print('Default 4 lines DDN6 array (length = {}):'.format(len(multiDDN)))
     print(multiDDN)
-
+    # Testing Tom's protocols:
+    multiTom1 = CreateTom(testCase=1)
+    print('Multiple methods array (Tom, case=1) (length = {} - {} injections):'.format(len(multiTom1), len(np.unique(multiTom1[:,:2], axis=0))))
+    print(multiTom1)
+    multiTom2 = CreateTom(testCase=2)
+    print('Multiple methods array (Tom, case=2) (length = {} - {} injections):'.format(len(multiTom2), len(np.unique(multiTom2[:,:2], axis=0))))
+    print(multiTom2)
+    multiFullTom = np.vstack([multiTom1, multiTom2])
+    print('Multiple methods array (Tom, case=3) (length = {}):'.format(len(multiFullTom)))
+    print(multiFullTom)
+    print('{} injections for the full array!'.format(len(np.unique(multiFullTom[:,:2], axis=0))))
--- Utilities.py
+++ Utilities.py
@@ -3,3 +3,17 @@ import numpy as np
 def SaveArrayTXT(filename='DefaultArrayName.txt', array=None):
     if array is not None:
         np.savetxt(filename,array,fmt='%d',delimiter='\t')
+    
+def SortArray(array=None, type='ABMN'):
+    if array is not None:
+        if type == 'ABMN':
+            array = array[array[:,3].argsort()]
+            array = array[array[:,2].argsort(kind='mergesort')]
+            array = array[array[:,1].argsort(kind='mergesort')]
+            array = array[array[:,0].argsort(kind='mergesort')]
+        if type == 'NMBA':
+            array = array[array[:,0].argsort()]
+            array = array[array[:,1].argsort(kind='mergesort')]
+            array = array[array[:,2].argsort(kind='mergesort')]
+            array = array[array[:,3].argsort(kind='mergesort')]
+    return array
