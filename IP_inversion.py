2afc9cce1abad6e1a50e1184f08ce80c450af77c
Hadrien Michel <hadrien.michel@uliege.be>
2022-05-04 13:24:01 +0200

    options list

--- IP_inversion.py
+++ IP_inversion.py
@@ -1,25 +1,86 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  9 14:57:24 2022
+'''Complete list of options for Inversion objects: 
+(from: https://www.pygimli.org/gimliapi/classGIMLI_1_1Inversion.html)
+    - setRelativeError(pg.Vector or float): set the relative error for all data points
+                                            (float) or points-by-points (pg.Vector). 
+                                            If set to 0.0, no data weigthing or correction.
+                                            DEFAULT = 0.01 (1%)
+    - setAbsoluteError(pg.Vector or float): set absolute error for all data points (float) 
+                                            or points-by-points (pg.Vector).
+                                            DEFAULT = not used
+    - setTransData(transformation): set a transformation for tha data (log for example).
+                                    DEFAULT = Identity
+    - setTransModel(transformation): set a transformation for tha model (log for example).
+                                     DEFAULT = Identity
+    - setVerbose(bool): set the verbose status.
+                        DEFAULT = False
+    - setDoSave(bool): set debug output.
+                       DEFAULT = False
+    - setLineSearch(bool): set line search optimization of the update.
+                           DEFAULT = True
+    - setBlockyModel(bool): set the constraint on the model to L1 instead of L2.
+                            DEFAULT = False
+    - setRobustData(bool): set the constraint on the data to L1 instead of L2.
+                           DEFAULT = False
+    - setLambda(float): set the value of the regularization parameter.
+                        DEFAULT = 50.0
+    - setLambdaFactor(float): set the multiplier for the regularization at each iteration 
+                              (see Marquardt scheme).
+                              DEFAULT = 1.0
+    - setLambdaMinimum(float): set minimum value of the regularization parameter.
+                               DEFAULT = 1.0
+    - setLocalRegularization(bool): set local regularization (true) or global (false).
+                                    DEFAULT = FALSE
+    - setOptimizeLambda(bool): optimize Lambda using the L-curve criteria.
+                               DEFAULT = False
+    - setMaxIter(int): set the maximum number of iterations for the inversion.
+                       DEFAULT = 20
+    - setMaxCGLSIter(int): set the maximum number of iterations for the conjugated
+                           gradient least square optimization at each iteration.
+                           DEFAULT = 200
+    - setCGLSTolerance(float): set the tolerance for CGLS (default scaling = -1).
+                               DEFAULT = -1.0
+    - stopAtChi1(bool): set the inversion to stop at Chi²=1 (stopping criteria).
+                        DEFAULT = True
+    - setDeltaPhiAbortPercent(float): set the minimum acceptable delta in the objective 
+                                      function between iterations (stopping criteria).
+                                      DEFAULT = 2.0
+    - setMarquardtScheme(float): set the inversion to follow a Marquardt scheme. The
+                                 input value is the damping factor that multiplies 
+                                 lambda at each iteration.
+                                 DEFAULT = 0.8
+    - setRecalcJacobian(bool): set the inversion to recalculate the Jacobian matrix
+                               at each iteration.
+                               DEFAULT = True
+    - setBroydenUpdate(bool): set the update to Broyden (scaling by transform function
+                              derivatives).
+                              DEFAULT = False
+    - setModel(pg.Vector): set the starting model for the inversion.
+                           (default to mean value)
+    - setReferenceModel(pg.Vector): set the reference model for the inversion
+                                    (default to starting model).
+    - setCWeight(pg.Vector): set the constraint weight vector (boundary control).
+    - setMWeight(pg.Vector): set the model weigth for each cells.
 
-@author: tomde
-"""
+This list of options is complete and adress directly the GIMLi API.
 
+The list of transfoms is available at https://www.pygimli.org/gimliapi/classGIMLI_1_1Trans.html
+
+'''
 
 import pybert as pb
 import pygimli as pg
 import numpy as np
 
 
-tdip=pb.TDIPdata(filename='AgatheData\PP9_ChessionAmont.ohm', verbose=True)
+tdip=pb.TDIPdata(filename='AgatheData\PP9_ChessionAmont_topo.ohm', verbose=True)
 data = tdip.data
 mgr = pb.ERTManager(data = tdip.data)
 
 
 #tdip.showRhoa()
+mesh = pg.meshtools.readTetgen('D:\Documents\dox@uliege\Article HydroGPhy\Profiles GPHY\DataFiles\Inversion\mesh\mesh.1')
 
-
-mesh = mgr.createMesh(data, quality = 33.6,  paraDX = 0.5) # , paraDepth = 50, paraMaxCellSize = 1,
+# mesh = mgr.createMesh(data, quality = 33.6,  paraDX = 0.5) # , paraDepth = 50, paraMaxCellSize = 1,
 
 
 # # Invert resistivity
@@ -43,7 +104,7 @@ tdip.res = inv.runChi1()
 tdip.coverage = ertMgr.coverage()
 tdip.pd = ertMgr.paraDomain
 
-tdip.showResistivity(cMap='jet')
+# tdip.showResistivity(cMap='jet')
 
 
 
@@ -73,11 +134,13 @@ INV.setRobustData(robustData)
 INV.setBlockyModel(blockyModel)
 INV.setDeltaPhiAbortPercent(1)
 
+
+
 tdip.m = INV.run()
 tdip.mafwd = INV.response()
 
 # tdip.invertMa(nr = 0, lam= 10, ma = tdip.data['ip']*0.001)
-tdip.showChargeability(cMap='jet',cMin = 0, cMax = 25)
+# tdip.showChargeability(cMap='jet',cMin = 0, cMax = 25)
 
 
 ### Export VTK 
--- changeOrientationMNT.py
+++ changeOrientationMNT.py
@@ -24,25 +24,29 @@ MNT = np.loadtxt('AgatheData\PP9_ChessionAmont_MNT.xyz',delimiter=' ')
 Electrodes = np.loadtxt('AgatheData\PP9_Electrodes.csv',skiprows=1,delimiter=',', usecols=(1,2,3))
 
 # Reverse the electrodes (optional):
-Electrodes = np.flipud(Electrodes)
+# Electrodes = np.flipud(Electrodes)
 
 MNT = MNT[MNT[:,-1] > 0, :] # Remove all unused points in MNT (marker = -9999)
 
 # Define the transform:
 # 1) Translation
-Electrodes[:,:-1] -= Electrodes[0,:-1]
-MNT[:,:-1] -= Electrodes[0,:-1]
+ElecInit = Electrodes[0,:-1]
+MNT[:,:-1] -= ElecInit
+Electrodes[:,:-1] -= ElecInit
 # 2) Rotation
 # Compute distance between the first and the last point (in XY plane):
 dist = np.linalg.norm(Electrodes[0,:-1]-Electrodes[-1,:-1])
 angle = angleBetween(Electrodes[-1,:-1], [dist, 0])
-rotation = rot.from_rotvec([0, 0, -angle])
+rotation = rot.from_rotvec([0, 0, angle])
 Electrodes = rotation.apply(Electrodes)
 MNT = rotation.apply(MNT)
+MNT = MNT[::10,:]
 # transformMatrix = np.linalg.lstsq(Electrodes[(0,-1),:-1], np.asarray([[0, 0],[dist, 0]]))
 # Electrodes[:,:-1] = np.matmul(Electrodes[:,:-1], transformMatrix[0])
 # MNT[:,:-1] = np.matmul(MNT[:,:-1], transformMatrix[0])
-plt.plot(Electrodes[:,0], Electrodes[:,1])
+
+plt.scatter(MNT[:,0], MNT[:,1])
+plt.plot(Electrodes[:,0], Electrodes[:,1], 'r')
 plt.show()
 
 ## Saving the electrodes positions and the MNT:
