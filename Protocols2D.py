a4634f122377f9ff628adb8bc50ceb22e8971297
Hadrien Michel <hadrien.michel@uliege.be>
2022-08-09 15:56:00 +0200

    Added Surface/Borehole arrays

--- Protocols2D.py
+++ Protocols2D.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Utilities import SaveArrayTXT, ArrayToXML, SampleReciprocals
+from Utilities import SaveArrayTXT, ArrayToXML, SampleReciprocals, SortArray
 
 def CreateDDN(nElec:int=64, nMax:int=6, parralelizeMin:int=None):
     '''This function creates a dipole-dipole array for an in-line measurement
@@ -57,21 +57,58 @@ def CreateGradient(nElec:int=64, s:int=7):
     array = np.asarray(array)
     return array
 
+def makeBorehole(array, nElecSurface:int=64, posBorehole:int=32.5, nElecBorehole:int=32):
+    addArray = []
+    for abmn in array:
+        A, B, M, N = abmn[0], abmn[1], abmn[2], abmn[3]
+        if (M > posBorehole) or (N > posBorehole):
+            Madd, Nadd = M, N
+            if M > posBorehole: Madd = M + np.floor(posBorehole)
+            if N > posBorehole: Nadd = N + np.floor(posBorehole)
+            addArray.append([A, B, Madd, Nadd])
+            if (A+B)/2 != posBorehole:
+                Aadd, Badd = A, B
+                if M < posBorehole: Madd = nElecSurface+1 - M
+                if N < posBorehole: Nadd = nElecSurface+1 - N
+                if A < posBorehole: Aadd = nElecSurface+1 - A
+                if B < posBorehole: Badd = nElecSurface+1 - B
+                addArray.append([Aadd, Badd, Madd, Nadd])
+    addArray = np.asarray(addArray)
+    array = SortArray(np.vstack((array, addArray)).astype(int))
+    return array
+
+
 if __name__=='__main__':
     n = 6
     nElec = 64
     DDNArray = CreateDDN(nMax = n, nElec = nElec, parralelizeMin=6)
     print('Default DDN6 array (length = {}):'.format(len(DDNArray)))
-    #print(DDNArray)
-    ArrayToXML(DDNArray)
-    DDNArrayReciprocals = SampleReciprocals(DDNArray)
-    print('The corresponding DDN6 sampled reciprocal array (length = {}):'.format(len(DDNArrayReciprocals)))
-    ArrayToXML(DDNArrayReciprocals)
+    print(DDNArray)
+    DDN6Bore = makeBorehole(DDNArray)
+    print('Default DDN6 with borehole array (length = {}):'.format(len(DDN6Bore)))
+    print(DDN6Bore)
+    SaveArrayTXT('DDN6Borehole.txt', DDN6Bore)
+    DDN6BoreRecip = SampleReciprocals(DDN6Bore)
+    SaveArrayTXT('DDN6BoreholeReciprocals.txt', DDN6BoreRecip)
+
     s = 7
     GradientArray = CreateGradient(s = s, nElec = nElec)
     print('Default Gradient (s=7) array (length = {})'.format(len(GradientArray)))
-    #print(GradientArray)
-    ArrayToXML(GradientArray)
-    GradientArrayReciprocals = SampleReciprocals(GradientArray)
-    print('The corresponding Gradient (s=7) sampled reciprocal array (length = {}):'.format(len(GradientArrayReciprocals)))
-    ArrayToXML(GradientArrayReciprocals)
+    GradientBore = makeBorehole(GradientArray)
+    print('Default DDN6 with borehole array (length = {}):'.format(len(GradientBore)))
+    print(GradientBore)
+    SaveArrayTXT('Gradient7Borehole.txt', GradientBore)
+    GradientBoreRecip = SampleReciprocals(GradientBore)
+    SaveArrayTXT('GradientBoreholeReciprocals.txt', GradientBoreRecip)
+    # ArrayToXML(DDNArray)
+    # DDNArrayReciprocals = SampleReciprocals(DDNArray)
+    # print('The corresponding DDN6 sampled reciprocal array (length = {}):'.format(len(DDNArrayReciprocals)))
+    # ArrayToXML(DDNArrayReciprocals)
+    # s = 7
+    # GradientArray = CreateGradient(s = s, nElec = nElec)
+    # print('Default Gradient (s=7) array (length = {})'.format(len(GradientArray)))
+    # #print(GradientArray)
+    # ArrayToXML(GradientArray)
+    # GradientArrayReciprocals = SampleReciprocals(GradientArray)
+    # print('The corresponding Gradient (s=7) sampled reciprocal array (length = {}):'.format(len(GradientArrayReciprocals)))
+    # ArrayToXML(GradientArrayReciprocals)
